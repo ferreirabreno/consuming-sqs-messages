ba0cbce154e3d5a656ccac05a7dee513d9f13a09
breno <breno@pop-os.localdomain>
2023-02-09 15:37:35 -0300

    chore: initial commit

---
+++ src/main/java/dev/breno/consumingsqsmessages/ConsumingSqsMessagesApplication.java
@@ -0,0 +1,13 @@
+package dev.breno.consumingsqsmessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingSqsMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingSqsMessagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/breno/consumingsqsmessages/ConsumingSqsMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.breno.consumingsqsmessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingSqsMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
